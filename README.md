c768b1b3bea80e0ef7ded912d774f1df26bdef5e
Bryans01 <Steve.h.bryan@gmail.com>
2023-06-04 16:56:37 +0100

    Updating README
    Updating README

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# eng_ai
-Generative AI Engineering Drawings
+# ENG-AI
+
+ENG-AI is an open-source Python application that uses the OpenAI API to enhance images based on a given prompt.
+
+## Installation
+
+# Clone the repository:
+git clone https://github.com/<your-github-username>/eng-ai.git
+
+# Create a virtual environment and activate it:
+python3 -m venv venv
+source venv/bin/activate
+
+# Install the dependencies:
+pip install -r requirements.txt
+
+# Set your OpenAI API key:
+export OPENAI_API_KEY=your-api-key
+
+# Usage
+# To use the application, call the enhance_drawing function with the paths to the input and output images, and the prompt:
+
+from app import enhance_drawing
+
+enhance_drawing('input.png', 'output.png', 'Change the design to improve efficiency and usability')
+
+# License
+# This project is licensed under the terms of the MIT license.
---
+++ app.py
@@ -0,0 +1,44 @@
+import openai
+import requests
+from PIL import Image
+from io import BytesIO
+import os
+
+
+openai.api_key = os.getenv('OPENAI_API_KEY')
+#openai.api_key = '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'
+
+def enhance_drawing(input_image_path, output_image_path, prompt):
+    # Read the image file from disk and resize it
+    image = Image.open(input_image_path)
+    width, height = 1024, 1024  # adjust size as needed
+    image = image.resize((width, height))
+
+    # Convert the image to a BytesIO object
+    byte_stream = BytesIO()
+    image.save(byte_stream, format='PNG')
+    byte_array = byte_stream.getvalue()
+
+    try:
+        # Edit the image based on the prompt
+        response = openai.Image.create_edit(
+            image=byte_array,
+            n=1,
+            size="1024x1024",
+            prompt=prompt
+        )
+
+        # Get the URL of the created image
+        image_url = response['data'][0]['url']
+
+        # Save the new image to the specified path
+        with open(output_image_path, 'wb') as f:
+            f.write(requests.get(image_url).content)
+        print(f"Saved new image to {output_image_path}")
+    except openai.error.OpenAIError as e:
+        print(e.http_status)
+        print(e.error)
+
+# Call the function with the path to the input image, output image path, and a prompt
+enhance_drawing('C:\\Stephen\\Coding\\Image 1\\PFD.png', 'C:\\Stephen\\Coding\\Image 1\\PFD_out.png', 'Change the design to improve efficiency and usability')
+
